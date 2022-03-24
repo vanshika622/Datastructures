493b441a6f8adb34980ecfe60ff66715d5b99999
vanshika jain <vanshikajain622@gmail.com>
2022-03-24 19:26:27 +0530

    stck full linkedlist implementation

--- stack/linkedlistimplementation.cpp
+++ stack/linkedlistimplementation.cpp
@@ -20,22 +20,64 @@ void push(int data){
     top=newNode;
 
 }
+void pop(){
+	if(top==NULL){
+		cout<<"Stack is Empty\n";
+		return;
+	}
+	struct node* temp=top;
+	top=top->link;
+	temp->link=NULL;
+	delete(temp);
+	cout<<"Element is deleted\n";
+	
+}
+int isEmpty(){
+	if(top==NULL){
+		return 1;
+	}
+	else 
+	return 0;
+}
+int peek(){
+	if(top==NULL){
+		cout<<"Stack is Empty\n";
+		return 0;
+	}
+	return top->data;
+}
+void printReverse(node* head)
+{
+    // Base case
+    if (head == NULL)
+    return;
+ 
+    // print the list after head node
+    printReverse(head->link);
+ 
+    // After everything else is printed, print head
+    cout << head->data << " ";
+    
+}
 void print(){
     struct node* temp;
     temp=top;
+    if(temp==NULL){
+    	cout<<"Stack is Empty\n";
+	}
     cout<<"The stack elements are:";
-    while(temp){
-        cout<<temp->data;
-        temp=temp->link;
-    }
-    cout<<"\n";
+     printReverse(temp);
+  
 }
 int main(){
     int choice, data;
     while(1){
        cout<<"1. Push\n";
-        cout<<"2. Print\n";
-         cout<<"3. Quit\n";
+        cout<<"2. Pop\n";
+        cout<<"3. Print\n";
+        cout<<"4. isEmpty\n";
+        cout<<"5. peek Element\n";
+         cout<<"6. Quit\n";
           cout<<"Enter Your choice:";
           cin>>choice;
 
@@ -46,9 +88,27 @@ int main(){
                push(data);
                break;
                case 2:
+               	pop();
+               	
+               	break;
+               case 3:
                print();
+               cout<<"\n";
                  break;
-                 case 3: exit(1);
+                 case 4:
+               if(isEmpty()){
+               	cout<<"stack is Empty\n";
+			   }
+			   else{
+			   	cout<<"Stack is not empty\n";
+			   }
+               
+                 break;
+                 case 5:
+               cout<<peek()<<"\n";
+              
+                 break;
+                 case 6: exit(1);
                  default:
                  cout<<"wrong choice";
                  break;
